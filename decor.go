2629ce31199d450a8c9c83b1b5dab362dd6fc135
Tobias Hort <mail@tobiashort.com>
2025-08-06 14:48:09 +0200

    change colors

--- decor.go
+++ decor.go
@@ -3,12 +3,12 @@ package ansi
 type Decor = string
 
 const (
-	Red    Decor = "\033[0;31m"
-	Green  Decor = "\033[0;32m"
-	Yellow Decor = "\033[1;33m"
-	Blue   Decor = "\033[1;34m"
-	Purple Decor = "\033[1;35m"
-	Cyan   Decor = "\033[1;36m"
+	Red    Decor = "\033[31m"
+	Green  Decor = "\033[32m"
+	Yellow Decor = "\033[33m"
+	Blue   Decor = "\033[34m"
+	Purple Decor = "\033[35m"
+	Cyan   Decor = "\033[36m"
 
 	Bold      Decor = "\033[1m"
 	Underline Decor = "\033[4m"
