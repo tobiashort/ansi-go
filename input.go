9261d3caac43615eb5f35965b6a0ec2f35ca802e
Tobias Hort <mail@tobiashort.com>
2025-08-12 18:04:30 +0200

    add more inputs

--- input.go
+++ input.go
@@ -1,8 +1,11 @@
 package ansi
 
 const (
-	InputKeyUp    = "\033[A"
-	InputKeyDown  = "\033[B"
-	InputKeyRight = "\033[C"
-	InputKeyLeft  = "\033[D"
+	InputCtrlC          = "\x03"
+	InputEscape         = "\033"
+	InputCarriageReturn = "\x0A"
+	InputKeyUp          = "\033[A"
+	InputKeyDown        = "\033[B"
+	InputKeyRight       = "\033[C"
+	InputKeyLeft        = "\033[D"
 )
