53004005edcf6f8e65330f84eecea37c00b142a8
Tobias Hort <mail@tobiashort.com>
2025-08-29 17:00:31 +0200

    add backspace

--- input.go
+++ input.go
@@ -1,14 +1,15 @@
 package ansi
 
 const (
-	InputCtrlC    = "\x03"
-	InputEscape   = "\033"
-	InputCR       = "\x0A"
-	InputLF       = "\x0D"
-	InputCRLF     = "\x0A\x0D"
-	InputSpace    = "\x20"
-	InputKeyUp    = "\033[A"
-	InputKeyDown  = "\033[B"
-	InputKeyRight = "\033[C"
-	InputKeyLeft  = "\033[D"
+	InputCtrlC     = "\x03"
+	InputEscape    = "\033"
+	InputCR        = "\x0A"
+	InputLF        = "\x0D"
+	InputCRLF      = "\x0A\x0D"
+	InputSpace     = "\x20"
+	InputBackSpace = "\x08"
+	InputKeyUp     = "\033[A"
+	InputKeyDown   = "\033[B"
+	InputKeyRight  = "\033[C"
+	InputKeyLeft   = "\033[D"
 )
